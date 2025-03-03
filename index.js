539ae93ef29ee49fffb1324e8ea7075fc01dcc35
root <root@IgnacioLaptop.>
2025-03-03 17:20:40 +0100

    progress in Render

---
+++ index.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const cool = require("cool-ascii-faces")
+const app = express()
+const PORT = process.env.PORT || 16079
+
+app.use("/about",express.static("./public/about.html"))
+
+app.get("/",(request,response)=>{
+    response.send(`Servidor del <a href="/about">grupo 10</a><br>
+        <a href="/cool">Cool</a><br>
+        `)
+})
+
+app.get("/cool",(request, response)=>{
+    response.send(cool());
+});
+
+app.listen(PORT,()=>{
+    console.log(`Server running on ${PORT}`)
+})
