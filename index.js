405eef096d67c5a6c1e585c230ff23ab8ef68bc7
root <root@IgnacioLaptop.>
2025-03-04 19:31:07 +0100

    render changes

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ const cool = require("cool-ascii-faces")
 const app = express()
 const PORT = process.env.PORT || 16079
 
+const IOM = require('./index-IOM')
+
 app.use("/about",express.static("./public"))
 
 app.get("/",(request,response)=>{
@@ -17,4 +19,21 @@ app.get("/cool",(request, response)=>{
 
 app.listen(PORT,()=>{
     console.log(`Server running on ${PORT}`)
+})
+
+//Ignacio Ortiz Moreno
+
+// Filtrar por comunidad 
+let filtered = IOM.filter((v)=> v.autonomousCommunity === "Andalucía")
+
+//Suma de velocidad media
+
+let sum = filtered.reduce((acc,value) => acc + value.averageSpeedFined,0)
+
+// Media de velocidad en Andalucía 
+
+let average = sum / filtered.length
+
+app.get('/samples/IOM', (request,response)=> {
+    response.send(IOM())
 })
