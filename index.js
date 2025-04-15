369906c5d4574de7d625d3de7f3f9504b5576b7d
root <root@IgnacioLaptop.>
2025-04-15 12:28:35 +0200

    first changes l07

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 import { loadBackEnd } from "./src/back/radars-stats/index.js";
 import { loadBackendVCH } from "./src/back/accidents-stats/index.js";
 import { loadBackendJAM } from "./src/back/registrations-stats/index.js";
+import {handler} from "./src/front/build/handler.js";
 import express  from "express";
 import path from "path";
 const app = express()
@@ -16,16 +17,16 @@ const BASE_API="/api/v1";
 
 app.use("/about",express.static("./public"));
 app.use(express.json());
-app.get("/",(request,response)=>{
-    response.send(`Servidor del <a href="/about">grupo 10</a><br>
-        <a href="https://sos2425-10.onrender.com/api/v1/accidents-stats">API Víctor Cabrera Hurtado</a><br>
-        <a href="https://sos2425-10.onrender.com/api/v1/registrations-stats">API Jesús Aznar Montero</a><br>
-        <a href="https://sos2425-10.onrender.com/api/v1/radars-stats">API Ignacio Ortiz Moreno</a><br>
-        <a href="https://sos2425-10.onrender.com/api/v1/accidents-stats/docs">DOCS Víctor Cabrera Hurtado</a><br>
-        <a href="https://sos2425-10.onrender.com/api/v1/registrations-stats/docs">DOCS Jesús Aznar Montero</a><br>
-        <a href="https://sos2425-10.onrender.com/api/v1/radars-stats/docs">DOCS Ignacio Ortiz Moreno</a><br>
-        `)
-})
+// app.get("/",(request,response)=>{
+//     response.send(`Servidor del <a href="/about">grupo 10</a><br>
+//         <a href="https://sos2425-10.onrender.com/api/v1/accidents-stats">API Víctor Cabrera Hurtado</a><br>
+//         <a href="https://sos2425-10.onrender.com/api/v1/registrations-stats">API Jesús Aznar Montero</a><br>
+//         <a href="https://sos2425-10.onrender.com/api/v1/radars-stats">API Ignacio Ortiz Moreno</a><br>
+//         <a href="https://sos2425-10.onrender.com/api/v1/accidents-stats/docs">DOCS Víctor Cabrera Hurtado</a><br>
+//         <a href="https://sos2425-10.onrender.com/api/v1/registrations-stats/docs">DOCS Jesús Aznar Montero</a><br>
+//         <a href="https://sos2425-10.onrender.com/api/v1/radars-stats/docs">DOCS Ignacio Ortiz Moreno</a><br>
+//         `)
+// })
 
 
 app.listen(PORT,()=>{
@@ -39,3 +40,4 @@ app.listen(PORT,()=>{
 loadBackendJAM(app);
 loadBackendVCH(app);
 
+app.use(handler);
---
+++ src/front/src/routes/+page.svelte
@@ -0,0 +1,1 @@
+Mi primera pagina svelte desarrollada con nodejs (v2)
