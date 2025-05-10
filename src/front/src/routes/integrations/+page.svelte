69f40d659eb0635201682344b9c52906e75ad9ce
Jesús Aznar Montero <aznarmonterojesus@gmail.com>
2025-05-10 13:30:48 +0200

    integraciones con proxy

--- src/front/src/routes/integrations/+page.svelte
+++ src/front/src/routes/integrations/+page.svelte
@@ -12,7 +12,7 @@
     <a href="/accidents-stats/graph/g3"> Grafo g3 (Pyramid - Víctimas por provincia)<br><br></a>
     <a href="/accidents-stats/integrations"> Integración g15-precipitation-stats<br></a>
     <a href="/accidents-stats/integrations/g17-university-demands"> Integración g17-university-demands<br></a>
-    <a href="/accidents-stats/integrations/g19-sanctions-and-points-stats"> Integración g19-sanctions-and-points-stats<br></a>
+    <a href="/accidents-stats/integrations/VCH-g19-sanctions-and-points-stats"> Integración g19-sanctions-and-points-stats<br></a>
     <a href="/accidents-stats/integrations/g20-accidents-with-animals"> Integración g20-accidents-with-animals<br></a>
     <a href="/accidents-stats/integrations/g13-water-supply-improvements"> Integración g13-water-supply-improvements<br></a>
     
@@ -27,6 +27,7 @@
     <a href="/registrations-stats/graphs/g1"> Grafo g1 (Barras - Matriculaciones totales por año)<br></a>
     <a href="/registrations-stats/graphs/g2"> Grafo g2 (Circular - Matriculaciones totales por provincia)<br></a>
     <a href="/registrations-stats/graphs/g3"> Grafo g3 (Gráfico 3D - Matriculaciones nacionales)<br></a>
+    <a href="/registrations-stats/integrations/JAM-g12-annual-evolutions"> Grafo g1 (Barras - Matriculaciones totales por año)<br></a>
 </p>
 <p>
     GRAFOS  IGNACIO ORTÍZ (RADARS-STATS):<br>
