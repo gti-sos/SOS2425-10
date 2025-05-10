c58560a86adc9ed6ef16921ef36d8874db89e477
viccabhur10 <victorjch2003@gmail.com>
2025-05-10 09:25:02 +0200

    l11v2

---
+++ src/front/src/routes/grafos/+page.svelte
@@ -0,0 +1,27 @@
+<svelte:head>
+    <title>
+        Grafos
+    </title>
+</svelte:head>
+
+
+<p>
+    GRAFOS  VÍCTOR CABRERA (ACCIDENTS-STATS):
+    <a href="/accidents-stats/graph/g1"> Grafo g1 (Donut - Víctimas por provincia)</a>
+    <a href="/accidents-stats/graph/g2"> Grafo g2 (Barras - Víctimas por provincia)</a>
+    <a href="/accidents-stats/graph/g3"> Grafo g3 (Pyramid - Víctimas por provincia)</a>
+</p>
+
+<p>
+    GRAFOS  JESÚS AZNAR (REGISTRATIONS-STATS):
+    <a href="/registrations-stats/graphs/g1"> Grafo g1 (Barras - Matriculaciones totales por año)</a>
+    <a href="/registrations-stats/graphs/g2"> Grafo g2 (Circular - Matriculaciones totales por provincia)</a>
+    <a href="/registrations-stats/graphs/g3"> Grafo g3 (Gráfico 3D - Matriculaciones nacionales)</a>
+</p>
+<p>
+    GRAFOS  IGNACIO ORTÍZ (RADARS-STATS):
+    <a href="/radars-stats/graph/g1"> Grafo g1 (Donut - Víctimas por provincia)</a>
+    <a href="/radars-stats/graph/g2"> Grafo g2 (amCharts - Víctimas por provincia)</a>
+    <a href="/radars-stats/graph/g3"> Grafo g3 (Pyramid - Víctimas por provincia)</a>
+</p>
+
