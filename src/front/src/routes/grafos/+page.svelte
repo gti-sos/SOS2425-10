3a865f1fd8aeba4ce65f917667d85dd8cb46afb1
viccabhur10 <victorjch2003@gmail.com>
2025-05-10 10:29:20 +0200

    l11v2

--- src/front/src/routes/grafos/+page.svelte
+++ src/front/src/routes/grafos/+page.svelte
@@ -9,7 +9,17 @@
     GRAFOS  VÍCTOR CABRERA (ACCIDENTS-STATS):<br>
     <a href="/accidents-stats/graph/g1"> Grafo g1 (Donut - Víctimas por provincia)<br></a>
     <a href="/accidents-stats/graph/g2"> Grafo g2 (Barras - Víctimas por provincia)<br></a>
-    <a href="/accidents-stats/graph/g3"> Grafo g3 (Pyramid - Víctimas por provincia)<br></a>
+    <a href="/accidents-stats/graph/g3"> Grafo g3 (Pyramid - Víctimas por provincia)<br><br></a>
+    <a href="/accidents-stats/integrations/g15-precipitation-stats"> Integración g15-precipitation-stats<br></a>
+    <a href="/accidents-stats/integrations/g17-university-demands"> Integración g17-university-demands<br></a>
+    <a href="/accidents-stats/integrations/g19-sanctions-and-points-stats"> Integración g19-sanctions-and-points-stats<br></a>
+    <a href="/accidents-stats/integrations/g20-accidents-with-animals"> Integración g20-accidents-with-animals<br></a>
+    <a href="/accidents-stats/integrations/g13-water-supply-improvements"> Integración g13-water-supply-improvements<br></a>
+    
+    <a href="/accidents-stats/integrations/APIEXTERNA1"> APIEXTERNA1<br></a>
+    
+    <a href="/accidents-stats/integrations/APIEXTERNA2"> APIEXTERNA2<br></a>
+
 </p>
 
 <p>
