1dcfbf684af6e7ee220db7b27cb0c73b71ca8591
Jesús Aznar Montero <aznarmonterojesus@gmail.com>
2025-05-11 13:24:14 +0200

    commit /about

--- src/front/src/routes/about/+page.svelte
+++ src/front/src/routes/about/+page.svelte
@@ -2,5 +2,11 @@
     import { dev } from "$app/environment";
 </script>
 
+<p>
+    Vídeo Jesús Aznar Montero(REGISTRATIONS-STATS):<br>
+    <a href="https://youtu.be/2EjypDwkoh0?feature=shared"> Vídeo REGISTRATIONS-STATS<br></a>
+  
+
+</p>
 
 pagina de prueba (Development = { dev })
